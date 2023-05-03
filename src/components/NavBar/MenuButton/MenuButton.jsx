767bbe9dc926ab1571adbf2395216c1afcdaba3e
Valeriya Izotova <my@valerka-made.ru>
2023-05-03 17:53:52 +0300

    что-то

--- src/components/NavBar/MenuButton/MenuButton.jsx
+++ src/components/NavBar/MenuButton/MenuButton.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './MenuButton.css';
 
-const MenuButton = ({url, text}) => {
+const MenuButton = ({ url, text }) => {
   return (
-    <li className='nav__button' >
+    <li
+      className='nav__button'
+     
+      style={{ listStyleImage: `url('../Navigation/images/dashboard.svg')` }}
+    >
       <button className='item__button button'>{text}</button>
     </li>
   );
 };
 
 export default MenuButton;
+
+// list-style-image: url('../Navigation/images/dashboard.svg');
--- src/components/NavBar/Navigation/Navigation.jsx
+++ src/components/NavBar/Navigation/Navigation.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Navigation.css';
 
-const Navigation = ({children, type}) => {
+const Navigation = ({ children, type }) => {
   return (
     <nav className={`navigation navigation_type_${type}`}>
-      <ul className='main__navigation-list'>
+      <ul
+        className='main__navigation-list'
+        // style={{ listStyleImage: `url('./images/dashboard.svg')` }}
+      >
         {children}
       </ul>
     </nav>
